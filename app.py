13576e5e5d99a7613abd88cec27801ab424ff626
sxtdmWork <sxtdm.me@gmail.com>
2024-08-26 11:55:25 +0300

    initial

--- app.py
+++ app.py
@@ -1,45 +1,9 @@
-import json
-import requests
 import telebot
-
-bot = telebot.TeleBot('7375335495:AAEhGQDU4kynthpxmhiaEFZxxXFPmgWvKS0')
-
-keys = {
-    'биткоин': 'BTC',
-    'эфириум': 'ETH',
-    'доллар' : 'USD',
-}
+from config import keys, TOKEN
+from utils import ConvertionException, CryptoConverter
 
 
-class ConvertionException(Exception):
-    pass
-
-
-class CryptoConverter:
-    @staticmethod
-    def convert(quote: str, base: str, amount: str):
-        if quote == base:
-            raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
-
-        try:
-            quote_ticker = keys[quote]
-        except KeyError:
-            raise ConvertionException(f'Не удалось обработать валюту {quote}')
-
-        try:
-            base_ticker = keys[base]
-        except KeyError:
-            raise ConvertionException(f'Не удалось обработать валюту {base}')
-
-        try:
-            amount = float(amount)
-        except ValueError:
-            raise ConvertionException(f'Не удалось обработать кол-во {amount}')
-
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-        total_base = json.loads(r.content)[keys[base]]
-
-        return total_base
+bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['start', 'help'])
---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = '7375335495:AAEhGQDU4kynthpxmhiaEFZxxXFPmgWvKS0'
+
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар' : 'USD',
+}
